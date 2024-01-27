64b5fbd12bb406000dd590de1bd13bb58658b111
enzomazocorodrigues <46747655+enzomazocorodrigues@users.noreply.github.com>
2024-01-27 19:06:40 +0000

    build: install and config tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  console.log("App.js render!")
-  const [count, setCount] = useState(0)
-  const [anotherCount, setAnotherCount] = useState(0)
-
-  function handleClick() {
-    setCount((count) => count + 1)
-    setAnotherCount((anotherCount) => anotherCount + 2)
-  }
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={handleClick}>
-          count is {count} and {anotherCount}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <h1 className="text-3xl font-bold texte-slate-900 tracking-tight">Hello World!</h1>
 }
 
 export default App
