18de26b21fc5aef0ed41f674e840a7fd4803ac14
enzomazocorodrigues <46747655+enzomazocorodrigues@users.noreply.github.com>
2024-01-27 21:41:22 +0000

    feat: fetch data to Products table

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
       <h1 className="text-xl font-bold tracking-tight">Mini CRM</h1>
       <nav className="flex items-center gap-4">
         <a className="text-slate-500 text-sm font-medium" href="">Dashboard</a>
-        <a className="text-slate-500 text-sm font-medium" href="">Products</a>
+        <a className="text-sm text-slate-800 font-semibold" href="">Products</a>
         <a className="text-slate-500 text-sm font-medium" href="">Customers</a>
         <a className="text-slate-500 text-sm font-medium" href="">Sales</a>
       </nav>
---
+++ src/utils/currency.js
@@ -0,0 +1,11 @@
+function currency(value) {
+  const { format } = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  })
+  const amount = Number(value)
+
+  return format(amount)
+}
+
+export default currency
