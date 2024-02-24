374cdbc5f7ca87416dc7e3d3f3514158be67f0f3
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2024-02-24 10:37:34 -0300

    build: install and config react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Products from "./pages/Products.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Products />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Navbar />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
